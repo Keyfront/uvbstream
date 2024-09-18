0fbe340b74e32cce91dfb48917f8c9683d5528ad
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-09-18 20:01:59 +0200

    Create unban.js

---
+++ unban.js
@@ -0,0 +1,20 @@
+module.exports = {
+  data: {
+    name: 'pjs?unban',
+  },
+  async execute(message, args) {
+    if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('You do not have permission to unban members.');
+
+    const userId = args[0];
+    if (userId) {
+      try {
+        await message.guild.bans.remove(userId);
+        message.reply(`Successfully unbanned user with ID ${userId}`);
+      } catch (error) {
+        message.reply('Failed to unban user.');
+      }
+    } else {
+      message.reply('Please provide a user ID.');
+    }
+  },
+};
