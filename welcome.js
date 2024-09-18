8cc51304d6d293a53a36f3f72fc3d89d2a69244f
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-09-18 20:03:40 +0200

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,8 @@
+module.exports = {
+  data: {
+    name: 'pjs?welcome',
+  },
+  async execute(message) {
+    // This file handles welcome messages and does not need to be triggered by command
+  },
+};
