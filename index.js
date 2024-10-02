d4c2c46d6d5abcf33bda504b4fc1469e74835680
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-10-02 21:34:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,19 @@ const { spawn } = require('child_process');
   
   // Capture vidéo de la page
   const ffmpeg = spawn('ffmpeg', [
-  '-y',
-  '-f', 'image2pipe',
-  '-r', '30',
-  '-i', '-',
-  '-c:v', 'libx264',
-  '-pix_fmt', 'yuv420p',
-  '-preset', 'fast',
-  '-f', 'flv',
+   '-y',  // Pour écraser la sortie
+        '-f', 'image2pipe',
+        '-vcodec', 'mjpeg',
+        '-i', 'pipe:0', // Entrée pipe
+        '-vf', 'scale=1920:1080',  // Redimensionner la vidéo si nécessaire
+        '-c:v', 'libx264',  // Codec vidéo
+        '-preset', 'fast',  // Profil de vitesse de codage
+        '-b:v', '3000k',  // Débit vidéo
+        '-maxrate', '3000k',  // Débit max
+        '-bufsize', '6000k',  // Taille du tampon
+        '-pix_fmt', 'yuv420p',  // Format de pixels
+        '-g', '50',  // Intervalle d'image clé
+        '-f', 'flv', // Format de sortie
   `rtmp://a.rtmp.youtube.com/live2/sz87-dusv-psay-wsra-5xms`  // Utiliser la clé RTMP depuis les secrets GitHub
 ]);
 
